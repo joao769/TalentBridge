2d657a893eaae32955c905dc87afc2b691247803
João Victor <joaobarbosavictor132@gmail.com>
2024-11-29 21:47:53 -0300

    refactor: alterar fluxo de navegação no menu principal

--- TalentBridge/src/main/java/com/example/TalentBridgeApplication.java
+++ TalentBridge/src/main/java/com/example/TalentBridgeApplication.java
@@ -1,11 +1,12 @@
 package main.java.com.example;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import main.java.com.example.controller.CandidatoController;
+import main.java.com.example.controller.CurriculoController;
 import main.java.com.example.controller.EmpresaController;
 import main.java.com.example.controller.VagaController;
+import main.java.com.example.util.EntradaUtils;
 import main.java.com.example.view.CandidatoView;
 import main.java.com.example.view.EmpresaView;
 
@@ -13,43 +14,44 @@ public class TalentBridgeApplication {
 
     public static void main(String[] args) {
     	
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         CandidatoController candidatoController = new CandidatoController();
-        VagaController vagaController = new VagaController();
+        VagaController vagaController = new VagaController(); 
         EmpresaController empresaController = new EmpresaController();
-        CandidatoView candidatoView = new CandidatoView(candidatoController);
+        CurriculoController curriculoController = new CurriculoController();
+        CandidatoView candidatoView = new CandidatoView(candidatoController, curriculoController, vagaController);
         EmpresaView empresaView = new EmpresaView(empresaController, vagaController);
 
-
         while (true) {
-            try {
-                System.out.println("Escolha uma opção:");
-                System.out.println("1. Menu Empresa");
-                System.out.println("2. Menu Candidato");
-                System.out.println("0. Sair");
+            System.out.println("\r\n"
+                    + " _____     _            _    ______      _     _            \r\n"
+                    + "|_   _|   | |          | |   | ___ \\    (_)   | |           \r\n"
+                    + "  | | __ _| | ___ _ __ | |_  | |_/ /_ __ _  __| | __ _  ___ \r\n"
+                    + "  | |/ _` | |/ _ \\ '_ \\| __| | ___ \\ '__| |/ _` |/ _` |/ _ \\\r\n"
+                    + "  | | (_| | |  __/ | | | |_  | |_/ / |  | | (_| | (_| |  __/\r\n"
+                    + "  \\_/\\__,_|_|\\___|_| |_|\\__| \\____/|_|  |_|\\__,_|\\__, |\\___|\r\n"
+                    + "                                                  __/ |     \r\n"
+                    + "                                                 |___/      \r\n");
+            System.out.println("Escolha uma opção:");
+            System.out.println("1. Empresa");
+            System.out.println("2. Candidato");
+            System.out.println("0. Sair");
 
-                int escolha = scanner.nextInt();
-                scanner.nextLine(); // Limpa o buffer do scanner
+            int opcao = EntradaUtils.lerEntradaValidaInt(sc, "Opção:");
 
-                switch (escolha) {
-                    case 1:
-                        empresaView.showMenu();
-                        break;
-                    case 2:
-                        candidatoView.showMenu();
-                        break;
-                    case 0:
-                        System.out.println("Saindo...");
-                        scanner.close();
-                        return;
-                    default:
-                        System.out.println("Opção inválida. Tente novamente.");
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("Erro: Entrada inválida. Por favor, insira um número.");
-                scanner.nextLine();
-            } catch (Exception e) {
-                System.out.println("Erro inesperado: " + e.getMessage());
+            switch (opcao) {
+                case 1:
+                    empresaView.showMenu();
+                    break;
+                case 2:
+                    candidatoView.showMenu();
+                    break;
+                case 0:
+                    System.out.println("\nSaindo...");
+                    sc.close(); 
+                    return;
+                default:
+                    System.out.println("\nOpção inválida. Tente novamente.");
             }
         }
     }
